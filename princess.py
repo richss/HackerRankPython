0a9d246c8d427b7bfe13faf247544c1d101c6299
Richard Stansbury <stansbur@erau.edu>
2015-03-12 21:13:35 -0400

    Added comments like any good developer should

--- princess.py
+++ princess.py
@@ -7,6 +7,7 @@
 #  This is my first Python program beyond hello world
 
 
+# Brute Force Search for the bot 'm' and the princess 'p'
 def findPlayers(n, grid):
     for row in xrange(0, m):
         for col in xrange(0, m):
@@ -22,12 +23,15 @@ def findPlayers(n, grid):
     return [sRow, sCol, pRow, pCol]
 
 
+# Moves the bot to the princess following a simple zig-zag
+#  pattern
 def displayPathtoPrincess(n, grid):
 
     [row, col, pRow, pCol] = findPlayers(n, grid)
 
     while row != pRow or col != pCol:
 
+        #Row Move
         if row > pRow:
             print "UP"
             row -= 1
@@ -36,6 +40,7 @@ def displayPathtoPrincess(n, grid):
             print "DOWN"
             row += 1
 
+        #Column Move
         if col > pCol:
             print "LEFT"
             col -= 1
@@ -44,13 +49,15 @@ def displayPathtoPrincess(n, grid):
             print "RIGHT"
             col += 1
 
-
+#Setup
 m = input()
 
 grid = []
 for i in xrange(0, m):
     grid.append(raw_input().strip())
 
+
+#Run
 displayPathtoPrincess(m, grid)
 
 
